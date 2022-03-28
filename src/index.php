a8f641066b749f521f62d076ba4d2d8d6b529bc8
perrin446u <perrin446u@eut.univ-lorraine.fr>
2022-03-28 15:53:52 +0200

    début

--- src/index.php
+++ src/index.php
@@ -1,3 +1,20 @@
 <?php
+declare(strict_types=1);
+require ('vendor/autoload.php');
 
+use \games\model\Game;
+use \games\model\Company;
+use \games\model\Platform;
+use \games\model\Character;
 
+$db = new DB();
+$db->addConnection( parse_ini_file('src/conf/conf.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
+
+$liste = Game::where('name', 'like', '%Mario%') ->get();
+foreach ($liste as $chibre)
+{
+    echo $chibre->name;
+}
+$liste = Company::where('location_country', 'Japan') -> get();
